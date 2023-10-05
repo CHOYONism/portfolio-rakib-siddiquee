3355c08f0cc1949f2c405addea23bfb8b4d73bd0
Md Jobayer Al Mahmud Ahad <30753487+jobayerahad@users.noreply.github.com>
2023-10-06 02:11:42 +0600

    The changer added

--- src/components/Structure/Header/index.tsx
+++ src/components/Structure/Header/index.tsx
@@ -1,5 +1,6 @@
-import { Anchor, Image, Group, Title } from '@mantine/core'
 import Link from 'next/link'
+import { Anchor, Image, Group, Title, useComputedColorScheme, useMantineColorScheme, ActionIcon } from '@mantine/core'
+import { BsMoon as DarkIcon, BsSun as LightIcon } from 'react-icons/bs'
 
 const array = [
   { title: 'Home', url: '/' },
@@ -9,21 +10,34 @@ const array = [
   { title: 'Contact', url: '/contact-me' }
 ]
 
-const Header = () => (
-  <Group justify="space-between" pos="sticky" top={0} py="xs">
-    <Group gap="xs">
-      <Image src="/logo.png" alt="logo" height={48} width="auto" />
-      <Title size={24}>Rakib Siddiquee</Title>
-    </Group>
+const Header = () => {
+  const computedColorScheme = useComputedColorScheme('light', { getInitialValueInEffect: true })
+  const { setColorScheme } = useMantineColorScheme()
 
-    <Group>
-      {array.map(({ title, url }, index) => (
-        <Anchor component={Link} href={url} key={index}>
-          {title}
-        </Anchor>
-      ))}
+  return (
+    <Group justify="space-between" pos="sticky" top={0} py="xs">
+      <Group gap="xs">
+        <Image src="/logo.png" alt="logo" height={48} width="auto" />
+        <Title size={24}>Rakib Siddiquee</Title>
+      </Group>
+
+      <Group>
+        {array.map(({ title, url }, index) => (
+          <Anchor component={Link} href={url} key={index}>
+            {title}
+          </Anchor>
+        ))}
+
+        <ActionIcon
+          variant="light"
+          aria-label="color-theme"
+          onClick={() => setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light')}
+        >
+          {computedColorScheme === 'light' ? <DarkIcon /> : <LightIcon />}
+        </ActionIcon>
+      </Group>
     </Group>
-  </Group>
-)
+  )
+}
 
 export default Header
